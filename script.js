2a08b3383940bbdbce5ffa5f0da2f84bb843e4e4
Domán <domanmartin07@gmail.com>
2025-01-10 12:30:26 +0100

    0.1

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
+const lowerCase = "abcdefghijklmnopqrstuvwxyz";
+const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const numbers = "0123456789";
+const specChar = "§+!%/=();_*?@#<>[]";
+const passwd = [];
+
 // Generáló gomb működtetése:
 document.getElementById("generateBtn").onclick = handleClick;
 
 function handleClick() {
     const values = getInputValue();
+    getReadyPasswd(values);
+    console.log(values);
 }
 // Üzenetet küldő eljárás:
 function sendMessage(message) {
@@ -12,7 +20,33 @@ function sendMessage(message) {
 // Értékek kinyerése. Ha hiányzik érték az inputból, akkor üzenetet küldünk, input-ra helyezzük a fókszt, és null-t adunk vissza.
 function getInputValue() {
     const inputElements = document.querySelectorAll("input");
-    for (let element of inputElements) {
+    const values = {};
+    if (inputElements.length > 0) {
+        for (let index in inputElements) {
+            if (inputElements[index].value === "") {
+                sendMessage("Minden adat megadása szükséges!");
+                inputElements[index].focus();
+                return {};
+            }
+            //values.push(parseInt((input.value)));
+            values[inputElements[index].id] = inputElements[index].value;
+        }
+        return values;
+    }
+}
 
+function createPasswdPart(count, collection) {
+    for (let i = 0; i < count; i++){
+        passwd.push(collection[Math.floor(Math.random() * collection.length)]);
+    }
+    
+}
+
+function getReadyPasswd() {
+    const donePasswd = [];
+    for (let key in values) {
+        switch (key) {
+            case 
+        }
     }
 }
